1d443fa010f71407ac5c0952141510de4e4ef077
Cedrick Cyizere <cedrickcyizere@gmail.com>
2018-11-08 12:41:21 -0800

    Modified the splash screen Added the bottom menu

--- app/src/main/java/com/octavian/octaviantaskmanager/MainActivity.java
+++ app/src/main/java/com/octavian/octaviantaskmanager/MainActivity.java
@@ -1,7 +1,11 @@
 package com.octavian.octaviantaskmanager;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav=findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem Item) {
+                    Fragment selectedFragment=null;
+                    switch(Item.getItemId())
+                    {
+                        case R.id.Home:
+                            selectedFragment=new HomeFragment();
+                            break;
+                        case R.id.Calendar:
+                            selectedFragment=new CalendarFragment();
+                            break;case R.id.Settings:
+                        selectedFragment=new SettingsFragment();
+                        break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+
+                }
+            };
 }
--- app/src/main/java/com/octavian/octaviantaskmanager/SplashAcivity.java
+++ app/src/main/java/com/octavian/octaviantaskmanager/SplashAcivity.java
@@ -1,18 +1,27 @@
 package com.octavian.octaviantaskmanager;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashAcivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+      new Handler().postDelayed(new Runnable(){
+          @Override
+          public void run() {
+            Intent homeIntent=new Intent(SplashAcivity.this,MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+          }
+      },SPLASH_TIME_OUT);
+
+
     }
 }
